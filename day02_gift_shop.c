d355a56ec3b2c9b2e4c1b664aa897efba9ac1f83
Hrusheekesh Warnekar <192113352+hrushw@users.noreply.github.com>
2025-12-02 21:39:28 +0530

    day 2 part 2 brute force

--- day02_gift_shop.c
+++ day02_gift_shop.c
@@ -1,25 +1,49 @@
 #include <stdio.h>
 
 typedef unsigned long long ull;
-int main(void) {
+
+
+int part1(void) {
+	ull rf, rc, r, rl, ru, sum = 0;
+
+	while(scanf("%llu-%llu,", &rf, &rc) != EOF) {
+		ull p10 = 1;
+		while((p10 - 1) < (ru = (rf / p10))) p10 *= 10;
+		for(rl = (p10 / 10); rl < p10; ++rl) {
+			r = rl * (p10 + 1);
+			if(r >= rf && r <= rc) sum += r;
+		}
+	}
+
+	printf("Sum evaluates to: %llu\n", sum);
+	return 0;
+}
+
+int part2(void) {
 	ull rf, rc, r, rl, ru, sum = 0;
 
 	while(scanf("%llu-%llu,", &rf, &rc) != EOF) {
 		for(r = rf; r <= rc; ++r) {
-			ull p10 = 1;
+			for(ull p10 = 10; (p10 - 1) < r; p10 *= 10) {
+				rl = r % p10;
+				ru = r / p10;
 
-			while((rl = (r % p10)) < (ru = (r / p10)))
-				p10 *= 10;
+				if(!rl) continue;
+				if(!(rl / (p10 / 10))) continue;
+				while((ru % p10) == rl) ru /= p10;
+				if(ru) continue;
 
-			if(rl == ru && (rl / (p10 / 10))) {
-				if((sum + r) < sum)
-					return fprintf(stderr, "OVERFLOW WARNING!!!\n"), -1;
+				// printf("Found: %llu in range { %llu - %llu }\n", r, rf, rc);
 				sum += r;
-				// printf("FOUND: [%llu]\nIn range { %llu - %llu }\nSum evaluates to: %llu\n\n", r, rc, rf, sum);
+				break;
 			}
 		}
 	}
 
 	printf("Sum evaluates to: %llu\n", sum);
 	return 0;
+}
+
+int main(void) {
+	return part2();
 }
