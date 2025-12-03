f150201d3278f9179504eb40aef0b3eafebe6c9f
Hrusheekesh Warnekar <192113352+hrushw@users.noreply.github.com>
2025-12-03 11:38:33 +0530

    day 3 part 2 - general solution

--- day03_lobby.c
+++ day03_lobby.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int maxjoltage(unsigned char *bank, int banksz) {
+int maxjoltage_2(unsigned char *bank, int banksz) {
 	unsigned int i1 = 0, i0, i;
 
 	for(i = 1; bank[i+1]; ++i)
@@ -15,16 +15,37 @@ int maxjoltage(unsigned char *bank, int banksz) {
 	return (((bank[i1] - '0') * 10) + (bank[i0] - '0'));
 }
 
+unsigned long long maxjoltage(unsigned char *bank, int banksz, unsigned int n) {
+	unsigned long long jolt = 0;
+
+	unsigned int n_ = 0;
+	unsigned int i0 = 0, i;
+
+	while(n_ < n) {
+		for(i = i0 + 1; bank[i + (n - n_ - 1)]; ++i)
+			if(bank[i] > bank[i0])
+				i0 = i;
+
+		jolt *= 10;
+		jolt += (bank[i0] - '0');
+
+		i0 += 1;
+		n_ += 1;
+	}
+
+	return jolt;
+}
+
 int main(void) {
 	enum {BANKSZ_ = 1024};
 	unsigned char bank[BANKSZ_] = {0};
-	unsigned long sum = 0;
+	unsigned long long sum = 0;
 
 	int c;
 	unsigned int i = 0;
 	while((c = getchar()) != EOF) {
 		if(c == '\n') {
-			sum += maxjoltage(bank, i);
+			sum += maxjoltage(bank, i, 12);
 			i = 0;
 			continue;
 		}
@@ -33,6 +54,6 @@ int main(void) {
 		i ++;
 	}
 
-	printf("Final Joltage: %lu\n", sum);
+	printf("Final Joltage: %llu\n", sum);
 	return 0;
 }
