bd7b4361cd1ff2917cda9608eec553a8571923e7
Hrusheekesh Warnekar <192113352+hrushw@users.noreply.github.com>
2025-12-03 11:44:47 +0530

    remove redundant part 1 solution

--- day03_lobby.c
+++ day03_lobby.c
@@ -1,36 +1,16 @@
 #include <stdio.h>
 
-int maxjoltage_2(unsigned char *bank, int banksz) {
-	unsigned int i1 = 0, i0, i;
-
-	for(i = 1; bank[i+1]; ++i)
-		if(bank[i] > bank[i1])
-			i1 = i;
-
-	i0 = i1 + 1;
-	for(i = i1+2; bank[i]; ++i)
-		if(bank[i] > bank[i0])
-			i0 = i;
-
-	return (((bank[i1] - '0') * 10) + (bank[i0] - '0'));
-}
-
 unsigned long long maxjoltage(unsigned char *bank, int banksz, unsigned int n) {
 	unsigned long long jolt = 0;
-
-	unsigned int n_ = 0;
-	unsigned int i0 = 0, i;
+	unsigned int n_ = 0, i0 = 0, i;
 
 	while(n_ < n) {
 		for(i = i0 + 1; bank[i + (n - n_ - 1)]; ++i)
 			if(bank[i] > bank[i0])
 				i0 = i;
 
-		jolt *= 10;
-		jolt += (bank[i0] - '0');
-
-		i0 += 1;
-		n_ += 1;
+		jolt = jolt * 10 + (bank[i0] - '0');
+		++i0, ++n_;
 	}
 
 	return jolt;
@@ -45,7 +25,7 @@ int main(void) {
 	unsigned int i = 0;
 	while((c = getchar()) != EOF) {
 		if(c == '\n') {
-			sum += maxjoltage(bank, i, 12);
+			sum += maxjoltage(bank, i, 2);
 			i = 0;
 			continue;
 		}
