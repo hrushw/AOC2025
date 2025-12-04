adc727a41821cdda24c3c58111b81450498e1a60
Hrusheekesh Warnekar <192113352+hrushw@users.noreply.github.com>
2025-12-04 20:18:50 +0530

    day 4 part 2 solution (inefficient but works)

--- day04_printing_department.c
+++ day04_printing_department.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
 enum { LINESZ_ = 1024 };
-unsigned char _linearr[3][LINESZ_] = {{0}};
 typedef unsigned char* Line;
+typedef unsigned char Grid[LINESZ_][LINESZ_];
+
+int chkroll(char c) {
+	return (c == '@' || c == 'M');
+}
 
 unsigned int nhbd(Line l, Line lp, Line ln, int i) {
 	unsigned int n = 0;
@@ -22,6 +26,25 @@ unsigned int nhbd(Line l, Line lp, Line ln, int i) {
 	return n;
 }
 
+unsigned int nhbd2d(Grid g, int i, int j) {
+	unsigned int n = 0;
+	if(!i) return n;
+
+	if(chkroll(g[i-1][j  ])) n += 1;
+	if(chkroll(g[i+1][j  ])) n += 1;
+
+	if(chkroll(g[i  ][j+1])) n += 1;
+	if(chkroll(g[i+1][j+1])) n += 1;
+	if(chkroll(g[i-1][j+1])) n += 1;
+
+	if(!j) return n;
+	if(chkroll(g[i-1][j-1])) n += 1;
+	if(chkroll(g[i  ][j-1])) n += 1;
+	if(chkroll(g[i+1][j-1])) n += 1;
+
+	return n;
+}
+
 void swotch(Line *l1, Line *l2, Line *l3) {
 	Line ltmp = *l1;
 	*l1 = *l2;
@@ -32,7 +55,8 @@ void clear(Line l) {
 	for(int i = 0; i < LINESZ_; ++i) l[i] = 0;
 }
 
-int main(void) {
+unsigned int part1(void) {
+	static unsigned char _linearr[3][LINESZ_] = {{0}};
 	Line lprev = _linearr[0];
 	Line lcur = _linearr[1];
 	Line lnext = _linearr[2];
@@ -49,6 +73,37 @@ int main(void) {
 		clear(lnext);
 	} while(scanstatus != EOF);
 
+	return out;
+}
+
+void clearmarks(Grid g) {
+	for(unsigned int i = 1; g[i][0]; ++i)
+		for(unsigned int j = 0; g[i][j]; ++j)
+			if(g[i][j] == 'M') g[i][j] = '.';
+}
+
+unsigned int part2(void) {
+	static Grid _linearr = {{0}};
+	unsigned int i, j;
+	for(i = 1; scanf("%s\n", _linearr[i]) != EOF; ++i);
+
+	unsigned int work, out = 0;
+	do {
+		work = 0;
+		for(i = 1; _linearr[i][0]; ++i)
+			for(j = 0; _linearr[i][j]; ++j)
+				if(_linearr[i][j] == '@' && nhbd2d(_linearr, i, j) < 4) {
+					work += 1;
+					_linearr[i][j] = 'M';
+				}
+		clearmarks(_linearr);
+		out += work;
+	} while(work);
+	return out;
+}
+
+int main(void) {
+	unsigned int out = part2();
 	printf("Accessible rolls: %u\n", out);
 
 	return 0;
